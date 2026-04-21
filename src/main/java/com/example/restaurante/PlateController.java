76c4d5d8b5c178ccfe931371fe139ac646aa712d
rcanol <r.canol.2023@alumnos.urjc.es>
2026-04-21 15:30:25 +0200

    Estandarización y Robustez

--- src/main/java/com/example/restaurante/PlateController.java
+++ src/main/java/com/example/restaurante/PlateController.java
@@ -1,46 +1,46 @@
 package com.example.restaurante;
 
-import com.example.restaurante.model.Plato;
+import com.example.restaurante.model.Plate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
-@RequestMapping("/platos")
+@RequestMapping("/plates")
 
-public class PlatoController {
-    private Map<Long, Plato> platos = new HashMap<>();
-    private Long contador = 1L;
+public class PlateController {
+    private Map<Long, Plate> plates = new HashMap<>();
+    private Long count = 1L;
 
     @GetMapping
-    public Collection<Plato> getPlatos() {
-        return platos.values();
+    public Collection<Plate> getPlates() {
+        return plates.values();
     }
 
     @PostMapping
-    public Plato crearPlato(@RequestBody Plato plato) {
-        plato.setId(contador++);
-        platos.put(plato.getId(), plato);
-        return plato;
+    public Plate createPlate(@RequestBody Plate plate) {
+        plate.setId(contador++);
+        plates.put(plate.getId(), plate);
+        return plate;
     }
 
     @GetMapping("/{id}")
-    public Plato getPlato(@PathVariable Long id) {
-        return platos.get(id);
+    public Plate getPlate(@PathVariable Long id) {
+        return plates.get(id);
     }
 
     @DeleteMapping("/{id}")
-    public void borrarPlato(@PathVariable Long id) {
-        platos.remove(id);
+    public void errasePlate(@PathVariable Long id) {
+        plates.remove(id);
     }
 
     @PatchMapping("/{id}")
-    public Plato actualizarParcial(@PathVariable Long id, @RequestBody Plato nuevo) {
-        Plato plato = platos.get(id);
-        if (plato != null) {
-            if (nuevo.getNombre() != null) plato.setNombre(nuevo.getNombre());
-            if (nuevo.getPrecio() != 0) plato.setPrecio(nuevo.getPrecio());
+    public Plate uptdateParcial(@PathVariable Long id, @RequestBody Plate new) {
+        Plate plate = plates.get(id);
+        if (plate != null) {
+            if (new.getName() != null) plate.setName(new.getName());
+            if (new.getPrice() != 0) plate.setPrice(new.getPrice());
         }
-        return plato;
+        return plate;
     }
 }
