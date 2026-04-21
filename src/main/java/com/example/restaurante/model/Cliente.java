ea226e54e4bbd5b2fb447e9cdfe1de15804a6ae6
afernandezva2023 <a.fernandezva.2023@alumnos.urjc.es>
2026-04-21 14:59:19 +0200

    Actualización de variables

--- src/main/java/com/example/restaurante/model/Cliente.java
+++ src/main/java/com/example/restaurante/model/Cliente.java
@@ -1,23 +1,23 @@
 package com.example.restaurante.model;
 
-public class Cliente {
+public class Client {
     private Long id;
-    private String nombre;
+    private String name;
     private String email;
 
-    public Cliente() {}
+    public Client() {}
 
-    public Cliente(Long id, String nombre, String email) {
+    public Client(Long id, String name, String email) {
         this.id = id;
-        this.nombre = nombre;
+        this.name = name;
         this.email = email;
     }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
