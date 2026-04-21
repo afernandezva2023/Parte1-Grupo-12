667d53ef5c2b8bc456352a0ee4107375d754657d
afernandezva2023 <a.fernandezva.2023@alumnos.urjc.es>
2026-04-21 18:03:24 +0200

    Update OrderController.java

--- src/main/java/com/example/restaurante/OrderController.java
+++ src/main/java/com/example/restaurante/OrderController.java
@@ -17,7 +17,7 @@ import com.example.restaurante.model.Order;
 
 @RestController
 @RequestMapping("/orders")
-public class orderController {
+public class OrderController {
 
     private Map<Long, Order> orders = new HashMap<>();
     private Long count = 1L;
@@ -31,15 +31,15 @@ public class orderController {
     // POST CREAR
     @PostMapping
     public Order createOrder(@RequestBody Order order) {
-        order.setId(contador++);
-        order.put(order.getId(), order);
+        order.setId(count++);
+        orders.put(order.getId(), order);
         return order;
     }
 
     // GET POR ID
     @GetMapping("/{id}")
     public Order getOrder(@PathVariable Long id) {
-        return order.get(id);
+        return orders.get(id);
     }
 
     // DELETE
@@ -50,25 +50,25 @@ public class orderController {
 
     // PATCH (EDITAR)
     @PatchMapping("/{id}")
-    public Order updateParcial(@PathVariable Long id, @RequestBody Order new) {
+    public Order updateParcial(@PathVariable Long id, @RequestBody Order newOrder) {
 
-        Order order = orders.get(id);
+    Order order = orders.get(id);
 
-        if (order != null) {
+    if (order != null) {
 
-            if (new.getClientName() != null) {
-                order.setClientName(new.getClientName());
-            }
-
-            if (new.getPlates() != null) {
-                order.setPlates(new.getPlates());
-            }
-
-            if (new.getTotal() != null) {
-                order.setTotal(new.getTotal());
-            }
+        if (newOrder.getClientName() != null) {
+            order.setClientName(newOrder.getClientName());
         }
 
-        return orders;
+        if (newOrder.getPlates() != null) {
+            order.setPlates(newOrder.getPlates());
+        }
+
+        if (newOrder.getTotal() != null) {
+            order.setTotal(newOrder.getTotal());
+        }
+    }
+
+    return order;
     }
 }
